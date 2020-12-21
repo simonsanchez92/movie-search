88d1367d5c7cde61977fcc8504a23f91a16e2867
simonsanchez92 <simonsanchez.1992@gmail.com>
2020-12-20 21:44:25 -0300

    cleaning up script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const searchContainer = document.getElementById('search-container');
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const resultText = document.getElementById('search-text');
-// const searchResults = document.getElementById('main');
+
 
 const resultsContainer = document.getElementById('main');
 const mainResults = document.getElementById('movies-container');
@@ -68,10 +68,6 @@ const showMovies = async (movies, url)=>{
     `
 
     //Insert search result text
-
-    // resultText.innerText = `Results for '${searchText}'` 
-  
-    // searchResults.appendChild(resultText);
     
     //Map over items and inject in UI
       
@@ -203,161 +199,6 @@ const getPrevPage = (url)=>{
         getMovies(query);       
     }  
 }
-
-
-
-// async function getAllData() {
-//     // moviesResult = [];
-//     const res = await fetch(`http://www.omdbapi.com/?s=${searchText}&apikey=9bdd7c5c`);
-//     const data = await res.json();
-
-//     console.log(data.Response)
-
-
-    
-            
-//             if (data.Response !== 'False') {
-//                 moviesResult.push(data);
-              
-//                 let results= [];
-//                 moviesResult[0].Search.forEach((movie, i)=>{results.push(movie)});
-//                 moviesResult = results;
-//                 console.log(moviesResult);
-//                 populateUI(moviesResult)
-//             }
-    
-    
-// }
-
-
-
-// function populateUI(results) {
-
-//     //Insert search result text
-
-//     resultText.innerText = `Results for '${searchText}'` 
-  
-//     searchResults.appendChild(resultText);
-    
-//     //Map over items and inject in UI
-      
-//     mainResults.innerHTML = results.map((movie, i)=>{
-//         // currentMovies.push(movie);
-//         return `<div class="movie-card">
-//         <i class="fas fa-tv"></i>
-//         <h3>${movie.Title}</h3>
-//         <div class="movie-img-container">
-//         <img src="${movie.Poster === 'N/A' ? 'img/default.jpg' : movie.Poster}" alt="${movie.Title}">
-//         </div> 
-//         <div class="card-overlay"> <button onclick="getMovieDetail(${i})">See More...</button></div>
-//         </div>`
-//     }).join(""); 
-// }
-
-
-
-//Detail page
-
-// let currentMovie;
-// function getMovieDetail(movieIndex) {
-//     let title = moviesResult[movieIndex].Title;
-  
-//     fetch(`http://www.omdbapi.com/?t=${title}&apikey=9bdd7c5c`)
-//     .then(res=> res.json())
-//     .then(data=>{
-//         currentMovie = data;
-//         showMovieDetail(data);
-       
-//     });
-    
-// searchResults.style.display = 'none';
-// }
-
-
-// function showMovieDetail(data) {
-
-//     if (data.Title === undefined) {
-//         // readPlotBtn.style.display = 'none';
-//         // goBackBtn.style.visibility = 'hidden';
-//     } else {
-       
-//         readPlotBtn.style.display = 'block';
-        
-//         movieDetailContainer.innerHTML = `
-        
-//         <div class='results-info'>
-//         <div class='movie-data'>
-//         <h3 class='movie-title'>${data.Title}<i class="fas fa-film"></i></h3>
-//         <ul class='movie-data-list'>
-//             <li><i class="fas fa-star"></i>imdbRating: ${data.imdbRating}</li>
-//             <li><i class="fas fa-star"></i>Year: ${data.Year}</li>
-//             <li><i class="fas fa-star"></i>Director: ${data.Director}</li>
-//             <li><i class="fas fa-star"></i>Featuring: ${data.Actors}</li>         
-//             <li><i class="fas fa-star"></i>Country: ${data.Country}</li>
-
-//             <li><i class="fas fa-star"></i>Genre: ${data.Genre}</li>
-//             <li><i class="fas fa-star"></i>BoxOffice: ${data.BoxOffice}</li>
-//         </ul>
-//         <button class='return-btn' id='return-btn' onclick=returnHome()><i class="fas fa-undo-alt return-btn-icon"></i></button>
-//         <button class='read-plot' id='read-plot' onclick=readPlot()>Read plot</button>
-        
-//         </div>
-        
-//         ${data.Poster !== 'N/A' ? `<div class="img-container">
-//         <img src=${data.Poster} alt='${data.Title}'></img>
-//         </div>` : ''}
-        
-//         </div>`
-//         goBackBtn.style.visibility = 'flex';
-//         movieDetailEl.style.display = 'flex';
-//     }
-   
-    
-// }
-
-// function readPlot() {
-  
-// let currentTitle = currentMovie.Title;
-   
-
-//     fetch(`http://www.omdbapi.com/?t=${currentTitle}&apikey=9bdd7c5c`)
-//         .then(res => res.json())
-//         .then(data => {
-
-//             movieDetailEl.classList.add('plot');
-//             movieDetailContainer.innerHTML = `
-        
-//         <div class='results-info'>
-//         <div class='movie-data'>
-//         <h3>${data.Title}<i class="fas fa-film"></i></h3>
-        
-//         <p class='plot-text'>${data.Plot}<p>
-        
-//         <button class='go-back-btn' id='go-back-btn' onclick=goBack()>Go back</button>
-//         <button class='return-btn' id='return-btn' onclick=returnHome()><i class="fas fa-undo-alt return-btn-icon"></i></button>
-//         </div>
-//         </div>
-        
-//         ${data.Poster !== 'N/A' ? `<div class="img-container">
-//         <img src=${data.Poster} alt='${data.Title}'></img>
-//         </div>` : ''}     
-        
-//         `
-//         })
-// }
-
-// function goBack() {
-//     console.log(currentMovie);
-//     movieDetailContainer.innerHTML = '';
-//     showMovieDetail(currentMovie);
-//     movieDetailEl.classList.remove('plot');
-//     // goBackBtn.style.visibility = 'hidden';
-//     readPlotBtn.style.visibility = 'visible';
-// }
-
-
-
-
 
 
 
